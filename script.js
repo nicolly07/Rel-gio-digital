3172fad406d72b90f092c0976d1f8ad0a5191ca8
nicolly07 <130067219+nicolly07@users.noreply.github.com>
2023-10-26 10:34:32 -0300

    relogio digital

---
+++ script.js
@@ -0,0 +1,13 @@
+<! DOCTYPE html> 
+<html lang="en">
+    <head>
+        <title>digit clock</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device=width" />
+        <link rel="stylesheet" href="styles.css" />
+        <link href="https://fonts.googleapis.com/css2?family=poppins:wght@500;700;900&display=swap" rel="styleshet">
+    </head>
+    <body>
+        <div id="myclockdisplay" class="clock" onload="showtime()"></div>
+    </body>
+</html>
